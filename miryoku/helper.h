3459fa3a7f9228dfae1ebd29116df13b06711e37
mutz3l <mutz3l@gmail.com>
2023-05-25 17:23:14 +0200

    added custom config for totem build

--- miryoku/helper.h
+++ miryoku/helper.h
@@ -147,11 +147,11 @@
         }; \
     };
 
-#define ZMK_UNICODE_SINGLE(name, L0, L1, L2, L3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+#define ZMK_UNICODE_SINGLE(name, L0, L1, L2) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2) \
     UC_MODMORPH(name, &name ## _lower, &none)
 
-#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
-    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
+#define ZMK_UNICODE_PAIR(name, L0, L1, L2, U0, U1, U2) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2) \
+    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2) \
     UC_MODMORPH(name, &name ## _lower, &name ## _upper)
