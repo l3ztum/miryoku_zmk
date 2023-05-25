5a1013de9d8c4448e945e3c7cea3bb18754c7dc1
mutz3l <mutz3l@gmail.com>
2023-05-25 17:23:14 +0200

    added custom config for totem build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -31,6 +31,7 @@
 #define MIRYOKU_NAV_INVERTEDT
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -38,17 +39,17 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_SYM, TAB),  U_LT(U_NUM, BSPC),   U_LT(U_NAV, SPACE), U_LT(U_MOUSE, RET),U_LT(U_MEDIA, ESC),U_NP,              U_NP
 
-#define MIRYOKU_LAYER_TAP \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
-&kp Y,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp H,             &kp M,             &kp SPACE,         &kp BSPC,          &kp DEL,           &kp ESC,           U_NP,              U_NP
+#define MIRYOKU_LAYER_NAV \
+&kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp INS,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp PG_DN,         &kp LEFT,          &kp DOWN,          &kp RIGHT,         &u_caps_word,      U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_NA,              &u_to_U_NAV,       &u_to_U_NUM,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DEL,           &kp RET,          &kp BSPC,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp SINGLE_QUOTE,  &kp LT,             &kp GT,           &kp DQT,           &kp SEMI,              &kp AMPS,          &kp U_NA,          &kp LBKT,         &kp RBKT,          &kp PRCNT,          \
 &kp EXCL,          &kp MINUS,          &kp PLUS,         &kp EQUAL,         &kp HASH,              &kp PIPE,          &kp COLON,         &kp LPAR,         &kp RPAR,          &kp QMARK,         \
 &kp CARET,         &kp SLASH,          &kp ASTRK,        &kp BSLH,          U_NA,                  &kp TILDE,         &kp DLLR,          &kp LBRC,         &kp RBRC,          &kp AT,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,                  &kp UNDER,         &de_ae,          &de_oe,          U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,                  &kp UNDER,         &kp KP_NUM,          U_NA,          U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MEDIA \
 U_RGB_HUI,         U_RGB_SAI,         &kp C_VOL_UP,      U_RGB_BRI,         U_RGB_TOG,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
