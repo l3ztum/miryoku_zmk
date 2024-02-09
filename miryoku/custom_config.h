3eec48f1c67460b71a6e76291073afef0714d2a1
mutz3l <mutz3l@gmail.com>
2024-02-09 11:49:28 +0100

    another correction, del not working

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,13 +16,13 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,               &kp U,              &kp I,              &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,               U_MT(LSHFT, J),     U_MT(LCTRL, K),     U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,               &kp M,              &kp COMMA,          U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              &kp HYP,           U_LT(U_FUN, DEL),  U_LT(U_SYM, TAB),  U_LT(U_NUM, BSPC), U_LT(U_NAV, SPACE),  U_LT(U_MOUSE, RET), U_LT(U_MEDIA, ESC), &kp MEH,           U_NP
+U_NP,              &kp HYP,           U_LT(U_FUN, DELETE),  U_LT(U_SYM, TAB),  U_LT(U_NUM, BSPC), U_LT(U_NAV, SPACE),  U_LT(U_MOUSE, RET), U_LT(U_MEDIA, ESC), &kp MEH,           U_NP
 
 #define MIRYOKU_LAYER_NAV \
 &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp INS,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp PG_DN,         &kp LEFT,          &kp DOWN,          &kp RIGHT,         &u_caps_word,      U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_NA,              &u_to_U_NAV,       &u_to_U_NUM,       &kp RALT,          U_NA,              \
-U_NP,              &trans,            &kp RET,           &kp DEL,           &kp BSPC,           U_NA,              U_NA,              U_NA,             &trans,            U_NP
+U_NP,              &trans,            &kp DELETE,        &kp TAB,           &kp BSPC,          U_NA,              U_NA,              U_NA,             &trans,            U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp SINGLE_QUOTE,  &kp LT,             &kp GT,           &kp DQT,           &kp SEMI,              &kp AMPS,          &kp U_NA,          &kp LBKT,         &kp RBKT,          &kp PRCNT,      \
